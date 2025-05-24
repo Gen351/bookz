8dc4afaf958602896e63b41d86812d22fc0552e5
Gen351 <117075425+Gen351@users.noreply.github.com>
2025-05-24 17:31:26 +0800

    Delete api directory KAAYO

--- api/config.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  // Allow cross‐origin if you need, e.g. res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Content-Type', 'application/json')
-  res.status(200).json({
-    url: process.env.SUPABASE_URL,
-    key: process.env.SUPABASE_ANON_KEY
-  })
-}
