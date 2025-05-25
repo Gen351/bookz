76fd896faaeb5070a651343d264dfe504ee1f886
Gen351 <117075425+Gen351@users.noreply.github.com>
2025-05-25 09:16:11 +0800

    Update app.js fixed the missing " ` "

--- js/app.js
+++ js/app.js
@@ -294,7 +294,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         // Create the comment itself
         const comment = document.createElement('p');
         comment.classList.add('comment');
-        comment.innerHTML = ${book.comment || '<em>No comments yet.</em>'}`;
+        comment.innerHTML = `${book.comment || '<em>No comments yet.</em>'}`;
         if (book.comment && book.comment.length > 100) {
             comment.innerHTML = `<strong>Notes:</strong> ${book.comment.substring(0, 100)}...`;
         }
